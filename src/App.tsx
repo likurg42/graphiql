af33cc3b15a73af3f8ae68685c6ec2d3cb36cd8a
231globus <231globus@gmail.com>
2023-05-06 21:57:18 +0300

    feat: added header, footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,20 @@ import AuthPage from './pages/AuthPage/AuthPage';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
 import HomePage from './pages/HomePage/HomePage';
 import PlaygroundPage from './pages/PlaygroundPage/PlaygroundPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/ide" element={<PlaygroundPage />} />
-      <Route path="/auth" element={<AuthPage />} />
-      <Route path="*" element={<ErrorPage />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/ide" element={<PlaygroundPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </>
   );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.header`
+  background-color: green;
+`;
+
+const Footer = () => (
+  <Wrapper>
+    <h1>Footer</h1>
+  </Wrapper>
+);
+
+export default Footer;
