834fd1b4e0fbf6e4f5229fad48267d68c583b5e3
likurg42 <likurg42@gmail.com>
2023-05-06 09:44:38 +0300

    chore: setup typescript & eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,70 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'react-refresh',
+    'import',
+  ],
+  rules: {
+    'comma-dangle': ['error', 'only-multiline'],
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        'js': 'always',
+        'ts': 'always',
+        'tsx': 'always,',
+      },
+    ],
+    'import/no-unresolved': 0,
+    /* Prettier */
+    // 'prettier/prettier': ['error', { 'endOfLine': 'auto' }],
+    /* Typescript */
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/ban-ts-comment': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-var-requires': 'off',
+    /* React */
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        'namedComponents': 'arrow-function',
+      },
+    ],
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    /* react-hooks */
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    /* react-refresh*/
+    'react-refresh/only-export-components': 'warn',
+  },
+};
