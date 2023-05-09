b838c9e60a018ff4a4c5e15e67d1c8ae478065b5
walqer <hakerstealer@gmail.com>
2023-05-09 23:58:39 +0600

    feat: add error icon to input

--- src/pages/SignUpPage/SignUpForm.tsx
+++ src/pages/SignUpPage/SignUpForm.tsx
@@ -24,9 +24,22 @@ const StyledForm = styled.form`
       margin-bottom: 20px;
     }
   }
+  & .input-wrap.error {
+    display: inline-block;
+    position: relative;
+    &:after {
+      position: absolute;
+      right: 5px;
+      top: 13px;
+      content: url('/error.svg');
+      display: inline-block;
+    }
+  }
   & input {
+    width: 100%;
     border: 1px solid #000000;
     padding: 13px 10px;
+    padding-right: 30px;
     font-size: 20px;
     border-radius: 0;
     margin-bottom: 10px;
@@ -34,6 +47,7 @@ const StyledForm = styled.form`
       outline: none;
     }
     &[type='checkbox'] {
+      width: 0;
       position: absolute;
       opacity: 0;
       z-index: -1;
@@ -60,7 +74,7 @@ const StyledForm = styled.form`
       text-decoration: underline;
     }
   }
-  & .error {
+  & .error-message {
     font-size: 12px;
     color: red;
     margin-bottom: 10px;
@@ -91,69 +105,82 @@ export const SignUpForm = () => {
   return (
     <StyledForm noValidate onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="username">Username</label>
-      <input
-        {...register('username', {
-          required: 'Username Address is required',
-          maxLength: {
-            value: 20,
-            message: 'The length of the username must be no more than 20 characters',
-          },
-          minLength: {
-            value: 3,
-            message: 'The length of the username must be more than 8 characters',
-          },
-        })}
-        id="username"
-        type="text"
-      />
-      {errors.username && <span className="error">{errors.username.message}</span>}
+      <span className={errors.username ? 'input-wrap error' : 'input-wrap'}>
+        <input
+          {...register('username', {
+            required: 'Username Address is required',
+            maxLength: {
+              value: 20,
+              message: 'The length of the username must be no more than 20 characters',
+            },
+            minLength: {
+              value: 3,
+              message: 'The length of the username must be more than 8 characters',
+            },
+          })}
+          id="username"
+          type="text"
+        />
+        {errors.username && <span className="error-message">{errors.username.message}</span>}
+      </span>
       <label htmlFor="email">Email</label>
-      <input
-        id="email"
-        type="email"
-        {...register('email', {
-          required: 'Email Address is required',
-          pattern: {
-            value:
-              /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu,
-            message: 'Enter correct email address',
-          },
-          minLength: {
-            value: 8,
-            message: 'The length of the email must be more than 8 characters',
-          },
-        })}
-      />
-      {errors.email && <span className="error">{errors.email.message}</span>}
+      <span className={errors.email ? 'input-wrap error' : 'input-wrap'}>
+        <input
+          id="email"
+          type="email"
+          {...register('email', {
+            required: 'Email Address is required',
+            pattern: {
+              value:
+                /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu,
+              message: 'Enter correct email address',
+            },
+            minLength: {
+              value: 8,
+              message: 'The length of the email must be more than 8 characters',
+            },
+          })}
+        />
+        {errors.email && <span className="error-message">{errors.email.message}</span>}
+      </span>
+
       <label htmlFor="password">Password</label>
-      <input
-        id="password"
-        type="password"
-        {...register('password', {
-          required: 'Password is required',
-          pattern: {
-            value: /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}/g,
-            message: 'At least one letter, one digit, one special character',
-          },
-          minLength: {
-            value: 8,
-            message: 'The length of the email must be more than 8 characters',
-          },
-        })}
-      />
-      {errors.password && <span className="error">{errors.password.message}</span>}
+      <span className={errors.password ? 'input-wrap error' : 'input-wrap'}>
+        <input
+          id="password"
+          type="password"
+          {...register('password', {
+            required: 'Password is required',
+            pattern: {
+              value: /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}/g,
+              message: 'At least one letter, one digit, one special character',
+            },
+            minLength: {
+              value: 8,
+              message: 'The length of the email must be more than 8 characters',
+            },
+          })}
+        />
+        {errors.password && <span className="error-message">{errors.password.message}</span>}
+      </span>
+
       <label htmlFor="repeat_password">Repeat Password</label>
-      <input
-        id="repeat_password"
-        type="password"
-        {...register('repeat_password', {
-          required: true,
-          validate: (value: string) => {
-            return watch('password') === value || 'Passwords should match!';
-          },
-        })}
-      />
-      {errors.repeat_password && <span className="error">{errors.repeat_password.message}</span>}
+      <span className={errors.repeat_password ? 'input-wrap error' : 'input-wrap'}>
+        <input
+          id="repeat_password"
+          type="password"
+          {...register('repeat_password', {
+            required: true,
+            validate: (value: string) => {
+              return watch('password') === value || 'Passwords should match!';
+            },
+          })}
+        />
+        {errors.repeat_password && (
+          <span className="error-message">{errors.repeat_password.message}</span>
+        )}
+      </span>
+
       <label htmlFor="terms">
         <input
           id="terms"
@@ -165,7 +192,7 @@ export const SignUpForm = () => {
           I agree to the <a href="/">Terms of User</a>
         </span>
       </label>
-      {errors.terms && <span className="error">{errors.terms.message}</span>}
+      {errors.terms && <span className="error-message">{errors.terms.message}</span>}
       <StyledWrapper>
         <Button type="submit" primary>
           Sign up
