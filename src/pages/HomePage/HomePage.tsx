1182351aa71936a8fb1437d750c86e920a38adb0
walqer <hakerstealer@gmail.com>
2023-05-11 15:08:57 +0600

    feat: add  redirect for logged users to console page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,24 +1,32 @@
 import styled from 'styled-components';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import Content from '../../components/Content';
 import Widget from './Widget';
+import { auth } from '../../firebase.ts';
 
 const HomeContent = styled.div`
   padding-top: 100px;
   max-width: 400px;
 `;
 
-const HomePage = () => (
-  <Content>
-    <HomeContent>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, aut molestiae quos aliquam
-        suscipit deserunt earum eaque, quis debitis, ab pariatur maiores eius culpa quae. Officia
-        fugiat quos tempore ratione.
-      </p>
-      <Widget />
-    </HomeContent>
-  </Content>
-);
+const HomePage = () => {
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+  if (user) navigate('/console');
+  return (
+    <Content>
+      <HomeContent>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, aut molestiae quos aliquam
+          suscipit deserunt earum eaque, quis debitis, ab pariatur maiores eius culpa quae. Officia
+          fugiat quos tempore ratione.
+        </p>
+        <Widget />
+      </HomeContent>
+    </Content>
+  );
+};
 
 export default HomePage;
